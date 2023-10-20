996a4e608adf967d2e69deeb80996d3ff61dbb30
ngourlay <ngourlay@rentsync.com>
2023-10-20 14:58:06 -0400

    first commit

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "Deploying application ..."
+
+export NVM_DIR=$HOME/.nvm;
+source $NVM_DIR/nvm.sh;
+
+# Pull the latest changes from GitHub
+cd ..
+git pull
+
+# Start the application through docker
+docker compose up -d
+
+echo "🚀 Application deployed!"
+
