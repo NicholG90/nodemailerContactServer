b8bd38b3604ab82d5f6d53c5f1ed958440c25312
ngourlay <ngourlay@rentsync.com>
2023-10-20 15:13:51 -0400

    updating server

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const cors = require("cors");
+// const cors = require("cors");
 const nodemailer = require("nodemailer");
 require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
-const server = http.createServer(app);
-
-server.listen(5000, () => {
+app.listen(5000, () => {
   console.log('HTTP Server running on port 5000');
 });
 
